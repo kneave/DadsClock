06e62f7dd11b5b18ada46000d9e7fad2c000b37e
unknown <keegan@jedibowler.com>
2015-01-03 13:26:29 +0000

    initial commit, basic communication to gps

---
+++ clock.ino
@@ -0,0 +1,119 @@
+//  Based on the code provided by Adafruit Industries:
+//  https://github.com/adafruit/Adafruit-GPS-Library
+
+//This code is intended for use with Arduino Leonardo and other ATmega32U4-based Arduinos
+
+#include <Adafruit_GPS.h>
+#include <SoftwareSerial.h>
+
+// Connect the GPS Power pin to 5V
+// Connect the GPS Ground pin to ground
+// Connect the GPS TX (transmit) pin to Arduino RX1 (Digital 8)
+// Connect the GPS RX (receive) pin to matching TX1 (Digital 7)
+w
+SoftwareSerial mySerial(8, 7);
+Adafruit_GPS GPS(&mySerial);
+
+// Set GPSECHO to 'false' to turn off echoing the GPS data to the Serial console
+// Set to 'true' if you want to debug and listen to the raw GPS sentences
+#define GPSECHO  false
+
+int buttonPin = 5;
+int satDetectedLed = 4;
+
+void setup()  
+{
+    
+  // connect at 115200 so we can read the GPS fast enough and echo without dropping chars
+  // also spit it out
+  Serial.begin(115200);
+  delay(5000);
+  Serial.println("Serial opened, program starting;");
+
+  //  set pin 4 to satellite detected LED
+  pinMode(satDetectedLed, OUTPUT); 
+  
+  //  set pin 5 to the acquire time button
+  pinMode(buttonPin, INPUT);
+  
+  // 9600 NMEA is the default baud rate for Adafruit MTK GPS's- some use 4800
+  GPS.begin(9600);
+  
+  // uncomment this line to turn on RMC (recommended minimum) and GGA (fix data) including altitude
+  GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
+  // uncomment this line to turn on only the "minimum recommended" data
+  //GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCONLY);
+  // For parsing data, we don't suggest using anything but either RMC only or RMC+GGA since
+  // the parser doesn't care about other sentences at this time
+  
+  // Set the update rate
+  GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ);   // 1 Hz update rate
+  // For the parsing code to work nicely and have time to sort thru the data, and
+  // print it out we wdon't suggest using anything higher than 1 Hz
+
+  // Request updates on antenna status, comment out to keep quiet
+  GPS.sendCommand(PGCMD_ANTENNA);
+
+  delay(1000);
+  // Ask for firmware version
+  mySerial.println(PMTK_Q_RELEASE);
+}
+
+uint32_t timer = millis();
+void loop()                     // run over and over again
+{
+  char c = GPS.read();
+  // if you want to debug, this is a good time to do it!
+  if ((c) && (GPSECHO))
+    Serial.write(c); 
+  
+  // if a sentence is received, we can check the checksum, parse it...
+  if (GPS.newNMEAreceived()) {
+    // a tricky thing here is if we print the NMEA sentence, or data
+    // we end up not listening and catching other sentences! 
+    // so be very wary if using OUTPUT_ALLDATA and trytng to print out data
+    //Serial.println(GPS.lastNMEA());   // this also sets the newNMEAreceived() flag to false
+  
+    if (!GPS.parse(GPS.lastNMEA()))   // this also sets the newNMEAreceived() flag to false
+      return;  // we can fail to parse a sentence in which case we should just wait for another
+  }
+
+  // if millis() or timer wraps around, we'll just reset it
+//  if (timer > millis())  timer = millis();
+
+//    if (millis() - timer > 2000) { 
+//      timer = millis(); // reset the timer
+//      GetTime();
+//    }
+  
+  if(digitalRead(buttonPin) == 1){
+    digitalWrite(satDetectedLed, HIGH);
+  }
+  else
+  {
+    digitalWrite(satDetectedLed, LOW);
+  }
+}
+
+void GetTime(){
+    
+    Serial.print("\nTime: ");
+    Serial.print(GPS.hour, DEC); Serial.print(':');
+    Serial.print(GPS.minute, DEC); Serial.print(':');
+    Serial.print("Date: ");
+    Serial.print(GPS.day, DEC); Serial.print('/');
+    Serial.print(GPS.month, DEC); Serial.print("/20");
+    Serial.println(GPS.year, DEC);
+    Serial.print("Fix: "); Serial.print((int)GPS.fix);
+    Serial.print(" quality: "); Serial.println((int)GPS.fixquality); 
+    Serial.print("Satellites: "); Serial.println((int)GPS.satellites);
+
+//    if (GPS.satellites >= 1) 
+//    {
+//      digitalWrite(4, HIGH); 
+//    }
+//    else
+//    {
+//      digitalWrite(4, LOW); 
+//    }
+}
